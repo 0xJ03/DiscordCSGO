31390e609daa1bfaf94ef1b05314c01baba9ab1d
0xJ03 <93682506+0xJ03@users.noreply.github.com>
2021-11-04 08:58:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,11 +9,26 @@ class MyClient(Discord.client):
     async def on_ready(self):
         print("Logged on as:", self.user)
     
-@bot.command()
-async def test(ctx, arg):
-if arg == "prisma":
-    await ctx.send("Opening", arg, "case!")
-    time.sleep(2)
+@client.command()
+async def help(ctx):
+    await ctx.send("Use .case to open a case!")
+    
+@client.command() # add more of the same item to make it more common
+@commands.cooldown(1, 4, commands.BucketType.user)
+async def case(ctx):
+    items = [
+    discord.Embed(title = "Wish Successfull", description = "You get 3 :star: Ebony Bow <:EbonyBow:857220938571317248> (Bow Weapon).", color = ctx.author.color), 
+    discord.Embed(title = "Wish Successfull", description = "You get 3 :star: Raven Bow <:RavenBow:857221178338574357> (Bow Weapon).", color = ctx.author.color),
+    discord.Embed(title = "Wish Successfull", description = "You get 3 :star: Recurve Bow <:RecurveBow:857221810932547594> (Bow Weapon).", color = ctx.author.color),
+    discord.Embed(title = "Wish Successfull", description = "You get 4 :star: Alley Hunter <:AlleyHunter:857262273805287434> (Bow Weapon).", color = ctx.author.color), 
+    discord.Embed(title = "Wish Successfull", description = "You get 4 :star: Blackcliff Warbow <:BlackcliffWarbow:857262272656441345> (Bow Weapon).", color = ctx.author.color),
+    discord.Embed(title = "Wish Successfull", description = "You get 4 :star: Compound Bow <:CompoundBow:857262272622493736> (Bow Weapon).", color = ctx.author.color),
+    discord.Embed(title = "Wish Successfull", description = "You get 5 :star: Lost Prayer to the Sacred Winds <:LostPrayertotheSacredWinds:857281184492552213> (Catalyst Weapon).", color = ctx.author.color),
+    discord.Embed(title = "Wish Successfull", description = "You get 5 :star: Memory of Dust <:MemoryofDust:857281131413635122> (Catalyst Weapon).", color = ctx.author.color),
+    discord.Embed(title = "Wish Successfull", description = "You get 5 :star: Skyward Atlas <:SkywardAtlas:857281156763746364> (Catalyst Weapon).", color = ctx.author.color)]
+    randomitem = random.choice(items)
+    await ctx.send(embed=randomitem)
+
     
     
     
