46f97716151198d33286cd87e3666b266b36041e
0xJ03 <93682506+0xJ03@users.noreply.github.com>
2021-11-04 08:52:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import asyncio
+import time
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='.')
@@ -6,9 +8,15 @@ bot = commands.Bot(command_prefix='.')
 class MyClient(Discord.client):
     async def on_ready(self):
         print("Logged on as:", self.user)
-        
-
-
+    
+@bot.command()
+async def test(ctx, arg):
+if arg == "prisma":
+    await ctx.send("Opening", arg, "case!")
+    time.sleep(2)
+    
+    
+    
 bot.run(token)
         
         
